add5b8d901274be18e2e621c1e5b2eab20a5ba3e
Jacky Li <ilikemacaronii@gmail.com>
2020-04-30 17:22:28 -0700

    functional slider for brightness

--- src/Palette.js
+++ src/Palette.js
@@ -4,13 +4,23 @@ import './Palette.css'
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {level: 500};
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+    changeLevel(level) {
+        this.setState({level});
+    }
     render() {
-        const colorBoxes = this.props.palette.colors[400].map(color =>  (
+        const {colors} = this.props.pallete;
+        const {level} = this.state;
+        const colorBoxes = colors[level].map(color =>  (
             <ColorBox background={color.hex} name={color.name}/>
         ));
         return (
             <div className="Palette">
-                {/* Navbar goes here */}
+                <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel}/>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>

9f5a7953a346e23d15ef30733a97fecab78fe4ea
Jacky Li <ilikemacaronii@gmail.com>
2020-05-18 17:52:12 -0700

    big refactor styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,8 +6,12 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from "@material-ui/core/Snackbar";
 import CloseIcon from "@material-ui/icons/Close";
 import IconButton from "@material-ui/core/IconButton";
+import {withStyles} from '@material-ui/styles';
+
 import 'rc-slider/assets/index.css';
-import './Navbar.css';
+import styles from './styles/NavbarStyles';
+// import './Navbar.css';
+
 
 class Navbar extends Component {
     constructor(props) {
@@ -27,23 +31,23 @@ class Navbar extends Component {
         this.setState(this.setState({open: false}));
     }
     render() {
-        const {level, changeLevel, showingAllColors } = this.props;
+        const {level, changeLevel, showingAllColors, classes} = this.props;
         const {format} = this.state;
         return (
-            <header className="Navbar">
-                <div className="logo">
+            <header className={classes.Navbar}>
+                <div className={classes.logo}>
                     <Link to="/">palettes</Link>
                 </div>
                 {showingAllColors && (
-                <div className="slider-container">
+                <div>
                     <span>Level:{level}</span>
-                    <div className='Navbar-slider'>
+                    <div className={classes.slider}>
                         <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel}/>
                     </div>
                 </div>)
                 }
                 
-                <div className="select-container">
+                <div className={classes.selectContainer}>
                     <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value="hex">HEX - #ffffff</MenuItem>
                         <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
@@ -65,4 +69,4 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
---
+++ src/NewPaletteForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NewPaletteForm extends Component {
+    render() {
+        return (
+            <div>
+                weewee
+            </div>
+        )
+    }
+}
+
+export default NewPaletteForm;

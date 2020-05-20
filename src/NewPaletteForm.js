6af748462cb5fedb3497bf9d48877e32ede6115f
Jacky Li <ilikemacaronii@gmail.com>
2020-05-20 00:21:12 -0700

    add Delete to PaletteForm

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -131,6 +131,10 @@ function NewPaletteForm(props) {
       props.history.push('/');
     }
 
+    const removeColor = (colorName) => {
+      setColors(colors.filter(color=>color.name !== colorName));
+    }
+
     return (
       <div className={classes.root}>
         <CssBaseline />
@@ -182,7 +186,11 @@ function NewPaletteForm(props) {
         <main className={clsx(classes.content, {[classes.contentShift]: open,})}>
             <div className={classes.drawerHeader} />
             {colors.map(color => 
-              <DraggableColorBox color={color.color} name={color.name}/>
+              <DraggableColorBox key={color.name}
+                                  color={color.color} 
+                                  name={color.name} 
+                                  handleClick={() => removeColor(color.name)}
+              />
             )}
         </main>
     </div>
